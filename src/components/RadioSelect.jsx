85ff5337808d475647dbe49bfcfc42537d82b90d
ADEK <adrianpodraza10@gmail.com>
2024-07-17 17:54:42 +0200

    added name prop

--- src/components/RadioSelect.jsx
+++ src/components/RadioSelect.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function RadioSelect({ label }) {
+function RadioSelect({ label, name }) {
   const [checked, setChecked] = useState(false);
   const handleChecked = function () {
     setChecked((state) => !state);
@@ -9,11 +9,16 @@ function RadioSelect({ label }) {
     <div
       className={
         checked
-          ? "bg-white flex max-w-[300px] gap-4 rounded-lg border-2 border-orange-primary px-4 py-3 accent-orange-primary"
-          : "bg-white flex max-w-[300px] gap-4 rounded-lg border-2 border-gray-100 px-4 py-3"
+          ? "flex max-w-[300px] gap-4 rounded-lg border-2 border-orange-primary bg-white px-4 py-3 accent-orange-primary"
+          : "flex max-w-[300px] gap-4 rounded-lg border-2 border-gray-100 bg-white px-4 py-3"
       }
     >
-      <input type="radio" checked={checked} onClick={handleChecked}></input>
+      <input
+        type="radio"
+        name={name}
+        checked={checked}
+        onClick={handleChecked}
+      ></input>
       <label className="font-bold">{label}</label>
     </div>
   );
