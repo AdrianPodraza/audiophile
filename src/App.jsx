f7d749ae9ec007f393207d3ed677d303d873f45d
ADEK <adrianpodraza10@gmail.com>
2024-07-24 15:56:52 +0200

    feat: add cart provider and hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import ProductDetail from "./Pages/ProductDetail";
 import { useEffect } from "react";
 import Cart from "./components/Cart";
 import Checkout from "./Pages/Checkout";
+import Summary from "./components/Summary";
+import { CartProvider } from "./context/CartContext";
+import { Toaster } from "react-hot-toast";
+import OrderModal from "./components/OrderModal";
 function App() {
   const { pathname } = useLocation();
 
@@ -15,18 +19,20 @@ function App() {
     window.scrollTo({ top: 0 });
   }, [pathname]);
   return (
-    <>
+    <CartProvider>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<OrderModal />} />
+        <Route path="/summary" element={<Summary />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/home" element={<MainPage />} />
         <Route path="/headphones" element={<Headphones />} />
         <Route path="/speakers" element={<Speakers />} />
         <Route path="/earphones" element={<Earphones />} />
         <Route path="/product/:slug" element={<ProductDetail />} />
       </Routes>
-    </>
+    </CartProvider>
   );
 }
 
