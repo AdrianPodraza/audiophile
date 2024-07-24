40bdfa744c13ed4a4edfabc8adfcd2cbd152c2fc
ADEK <adrianpodraza10@gmail.com>
2024-07-24 15:58:55 +0200

    fix: fix numberField

--- src/components/NumberField.jsx
+++ src/components/NumberField.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function NumberField() {
-  const [number, setNumber] = useState(1);
+function NumberField({ value, onChange, max = 20 }) {
+  const [number, setNumber] = useState(value);
+
+  useEffect(() => {
+    setNumber(value);
+  }, [value]);
+
   const handleIncrement = function () {
-    if (number >= 20) return;
-    setNumber((number) => number + 1);
+    if (number >= max) return;
+    const newValue = number + 1;
+    setNumber(newValue);
+    onChange(newValue);
   };
+
   const handleDecrement = function () {
     if (number <= 1) return;
-    setNumber((number) => number - 1);
+    const newValue = number - 1;
+    setNumber(newValue);
+    onChange(newValue);
   };
+
   return (
     <div className="flex h-[48px] min-w-[120px] items-center justify-center gap-7 bg-gray-200">
       <button
